f2b0c83d90ca899ddab997cc20e0b94f418987f8
anuragtech34 <prajapatianurag73240@gmail.com>
2024-10-08 09:34:25 +0530

    Created admin pannel

--- Baskit/src/components/NavBar/NavBar.jsx
+++ Baskit/src/components/NavBar/NavBar.jsx
@@ -6,7 +6,15 @@ export const NavBar = () => {
     <div className='flex justify-between items-center py-4 w-full'>
 
 <NavLink to={'/'} className='logo font-semibold text-3xl cursor-pointer'>BASKIT</NavLink>
-<div className='nav-options flex items-center gap-4 text-xl '>
+
+
+
+<div className='flex  gap-4 text-lg items-center justify-center '>
+  <p>Home</p>
+  <p>Shop</p>
+  <p>About</p>
+  <p>Contact us</p>
+  <p>|</p>
 <i class="fa-solid fa-magnifying-glass"></i>
 <i class="fa-solid fa-cart-shopping"></i>
 <i class="fa-solid fa-user"></i>
