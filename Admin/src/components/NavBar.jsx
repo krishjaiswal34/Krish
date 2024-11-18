25ffa7f9f8a4859b9b52d6399f825b6df2001c8a
anuragtech34 <prajapatianurag73240@gmail.com>
2024-11-18 08:33:36 +0530

    Minor chagnes

--- Admin/src/components/NavBar.jsx
+++ Admin/src/components/NavBar.jsx
@@ -6,13 +6,13 @@ const NavBar = () => {
   return (
     <div className='w-full flex justify-between py-4 text-start'>
         <div>
-            <h1 className='text-xl font-semibold'>Style Haven</h1>
+            <h1 className='font-semibold text-4xl  company-logo'>Style Haven</h1>
             <p>Admin pannel</p>
         </div>
        <div className='text-end'>
         <p className='break-all'>{logedInUser?.email}</p>
 
-       <button onClick={logOut} className=' rounded-full px-4 py-2  bg-[rgba(0,0,0,0.6)] text-white'>Logout</button>
+       <button onClick={logOut} className=' rounded-full px-4 py-2  bg-[rgba(0,0,0,0.6)] text-white hover:bg-red-700 transition duration-200 ease-in-out'>Logout</button>
        </div>
 
 
