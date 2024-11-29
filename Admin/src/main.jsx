78da33ecccd852d3c248c4912bf46f1c1bcc6be7
anuragtech34 <prajapatianurag73240@gmail.com>
2024-11-29 08:40:55 +0530

    modified import of AdminContext in main

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 import {RouterProvider} from 'react-router-dom'
 import { routes } from './routes/routes.jsx'
-import { AdminContextProvider } from './contexts/adminContext.jsx'
+import { AdminContextProvider } from './contexts/AdminContext.jsx'
 import { FirebaseAuthContext, FirebaseAuthContextProvider } from './contexts/FirebaseAuthContext.jsx'
 createRoot(document.getElementById('root')).render(
 
